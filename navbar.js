28ae9ed1aa69ca10aefe7b7704c9315a62c7b976
AdamJaneczech <anndyjan@seznam.cz>
2022-09-09 11:40:11 +0200

    navbar-jquery-progress

--- navbar.js
+++ navbar.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
       var scroll = $(window).scrollTop();
         if (scroll > 54) {
-          $(".navbar").css("background" , "blue");
+          $(".navbar").css("background" , "#272727");
         }
   
         else{
-            $(".navbar").css("background" , "white");   
+            $(".navbar").css("background" , "radial-gradient(circle, #272727 40%, transparent 10%), radial-gradient(circle, #272727 40%, transparent 10%), url(\"./Grafika/arduino_background.png\")");   
         }
     })
   })
